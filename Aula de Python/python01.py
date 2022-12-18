fa7265f9f962ee8cbbbc786dcfe7f7ce865630c7
Salurodev <salurodev@gmail.com>
2022-12-18 20:44:47 -0300

    aula py

---
+++ Aula de Python/python01.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    "Nome":["João Silva",
+            "Carlos Souza",
+            "Maria Ferreira"],
+    "Idade":[22, 35, 58],
+    "Sexo":["Masculino", "Masculino", "Feminino"]
+})
+print (df)
